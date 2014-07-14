9dc8f7d9aae62de16eac71668759a4ad2ca834e1
Leon <1196139850@qq.com>
2014-07-14 21:37:06 +0800

    第五次push，完成了部分js代码

--- src/test/Servlettest2.java
+++ src/test/Servlettest2.java
@@ -8,28 +8,16 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
- * 这是第一个servlet测试
+ * Created by leon on 2014/7/14.
  */
+@WebServlet("/test2")
 
-
-@WebServlet("/test")
-public class TestServlet extends HttpServlet {
-
-	@Override
+public class Servlettest2 extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 	}
 
-	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-
-		System.out.println("开始跳转");
-
-		System.out.println("https链接");
-
-//		response.sendRedirect("asd/test.html");
-
-//		System.out.println("跳转成功");
+		System.out.println("https链接在此生效！");
 	}
 }
