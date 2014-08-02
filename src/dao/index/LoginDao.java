6a4bdaec7635bbe33f4a36e81b221e650d25e595
leon <1196139850@qq.com>
2014-08-02 10:27:29 -0700

    这个类暂时性就做这些事情

--- src/dao/index/LoginDao.java
+++ src/dao/index/LoginDao.java
@@ -2,9 +2,15 @@ package dao.index;
 
 import beans.index.UserAccountBean;
 import dao.exception.NoSuchIDException;
+import sun.misc.Cache;
 import utils.db.GetConnection;
+import utils.db.GetRowSetFactory;
 import utils.db.ReleaseSource;
 
+import javax.sql.RowSet;
+import javax.sql.rowset.CachedRowSet;
+import javax.sql.rowset.RowSetFactory;
+import javax.sql.rowset.RowSetProvider;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -60,10 +66,10 @@ public class LoginDao {
 	 * 通过用户ID ，查询包括pic，HomePageName，UserInfo，UserProfile中的部分或者全部信息，并结合成一个结果集
 	 *
 	 * @param ID 用户后台标记ID
-	 * @return 包含用户设置的结果集；或者是null，当ID不存在
+	 * @return 包含用户设置的结果集；或者是null当获取时出现异常时
 	 */
 
-	public static ResultSet getUserSettings(long ID) {
+	public static CachedRowSet getUserSettings(long ID) {
 
 
 		Connection con = GetConnection.getMySqlConnection();
@@ -76,31 +82,98 @@ public class LoginDao {
 				"FROM UserProfile AS UI, HomePageName AS HP,HeadBgPic AS HB," +
 				"HeadPic AS HH WHERE UI.ID = ? AND HP.ID = ? AND HB.ID = ? AND HH.ID = ?";
 
+		CachedRowSet cachedRowSet = null;
 
 		try {
-			con.prepareStatement(sql);
 
+			ps = con.prepareStatement(sql);
 
+			ps.setLong(1, ID);
+			ps.setLong(2, ID);
+			ps.setLong(3, ID);
+			ps.setLong(4, ID);
 
+			resultSet = ps.executeQuery();
 
+			// 获取cacheRowSet并用ResultSet填充，下面这行代码只要发生异常，CacheRowSet一定是null
+			// 这个信息是可以作为DB是否异常的信息传递到上一层
+			cachedRowSet = buildCacheRawSet(resultSet);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			ReleaseSource.releaseSource(resultSet, ps);
+		}
 
+		// 这里返回要么是cacheRawSet，要么是null
+		return cachedRowSet;
+	}
 
+	/**
+	 * 返回用户关注的人的ID或者是null，当SQL查询出现异常
+	 *
+	 * @param ID 用户后台唯一标记
+	 * @return 可能包含信息的cacheRowSet或者null，当SQL查询出现异常时
+	 */
 
+	public static CachedRowSet getFollowingPeopleID(long ID) {
 
+		Connection con = GetConnection.getMySqlConnection();
 
+		PreparedStatement ps = null;
+		ResultSet resultSet = null;
 
+		String getFollowingSql = "SELECT following FROM following WHERE ID = ?";
 
+		CachedRowSet cachedRowSet = null;
 
+		try {
+			ps = con.prepareStatement(getFollowingSql);
+			ps.setLong(1, ID);
+			resultSet = ps.executeQuery();
 
+			cachedRowSet = buildCacheRawSet(resultSet);
 
 		} catch (SQLException e) {
 			e.printStackTrace();
+		} finally {
+			ReleaseSource.releaseSource(resultSet, ps);
 		}
 
-
-		return null;
+		return cachedRowSet;
 	}
 
+	/**
+	 * 返回用户好友的ID或者是null，当SQL查询出现异常
+	 *
+	 * @param ID 用户后台唯一标记
+	 * @return 可能包含信息的cacheRowSet或者null，当SQL查询出现异常时
+	 */
+
+	public static CachedRowSet getFriendsID(long ID) {
+
+		Connection con = GetConnection.getMySqlConnection();
+
+		PreparedStatement ps = null;
+		ResultSet resultSet = null;
+
+		String getFollowingSql = "SELECT friend FROM friends WHERE ID = ?";
+
+		CachedRowSet cachedRowSet = null;
+
+		try {
+			ps = con.prepareStatement(getFollowingSql);
+			ps.setLong(1, ID);
+			resultSet = ps.executeQuery();
+
+			cachedRowSet = buildCacheRawSet(resultSet);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			ReleaseSource.releaseSource(resultSet, ps);
+		}
+
+		return cachedRowSet;
+	}
 
 	/**
 	 * 检查用户登录时账号、密码是否正确
@@ -132,10 +205,7 @@ public class LoginDao {
 
 				String dbEmail = resultSet.getString("email");
 				String dbPassword = resultSet.getString("password");
-
-				if (dbEmail.equals(email) && dbPassword.equals(password)) {
-					return true;
-				}
+				return dbEmail.equals(email) && dbPassword.equals(password);
 			} else {
 				return false;
 			}
@@ -146,4 +216,14 @@ public class LoginDao {
 		}
 		return false;
 	}
+
+
+	private static CachedRowSet buildCacheRawSet(ResultSet resultSet) throws SQLException {
+		CachedRowSet cachedRowSet;
+		cachedRowSet = GetRowSetFactory.getRowSetFactory().createCachedRowSet();
+		cachedRowSet.populate(resultSet);
+		return cachedRowSet;
+	}
+
+
 }
