6ec9d00ae5882e336c8c4248ae8cd97057456920
leon <1196139850@qq.com>
2014-08-22 11:25:40 +0800

    添加了创建线程池代码

--- src/listener/TomcatOnListener.java
+++ src/listener/TomcatOnListener.java
@@ -1,22 +1,21 @@
 package listener;
 
+import utils.ThreadPoolUtil;
+
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
-import javax.servlet.http.HttpSessionAttributeListener;
-import javax.servlet.http.HttpSessionEvent;
-import javax.servlet.http.HttpSessionListener;
-import javax.servlet.http.HttpSessionBindingEvent;
 import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Enumeration;
+import java.util.concurrent.*;
 
 
 /**
  * author: 康乐
  * time: 2014/7/28
- * function: 用于tomcat启动的时候加载数据库等全局初始化任务
+ * function: 用于tomcat启动的时候加载数据库、创建线程池等全局初始化任务
  */
 
 
@@ -39,6 +38,17 @@ public class TomcatOnListener implements ServletContextListener{
 			e.printStackTrace();
 		}
 
+		System.out.println("创建线程池");
+
+		ThreadPoolExecutor cpuThreadPoolExecutor = new ThreadPoolExecutor(ThreadPoolUtil.CPUCORETHREADSNUMBER,ThreadPoolUtil.CPUMAXTHREADSNUMBER,
+				ThreadPoolUtil.CPUTHREADSKEEPALIVETIME,ThreadPoolUtil.CPUTHREADSTIMEUNIT,ThreadPoolUtil.CPUTHREADSQUEUE,ThreadPoolUtil.HANDLER);
+
+		ThreadPoolUtil.setCpuThreadPoolExecutor(cpuThreadPoolExecutor);
+
+		ThreadPoolExecutor ioThreadPoolExecutor = new ThreadPoolExecutor(ThreadPoolUtil.IOCORETHREADSNUMBER,ThreadPoolUtil.IOMAXTHREADSNUMBER,
+				ThreadPoolUtil.IOTHREADSKEEPALIVETIME,ThreadPoolUtil.IOTHREADSTIMEUNIT,ThreadPoolUtil.IOTHREADSQUEUE,ThreadPoolUtil.HANDLER);
+
+		ThreadPoolUtil.setIoThreadPoolExecutor(ioThreadPoolExecutor);
 	}
 
 
