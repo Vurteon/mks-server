2809f1b230d8cbb100f4c14517a4d5de54f3eee3
leon <1196139850@qq.com>
2014-08-24 16:02:31 +0800

    存储part完善

--- src/model/uploadpart/SavePart.java
+++ src/model/uploadpart/SavePart.java
@@ -1,104 +1,165 @@
 package model.uploadpart;
 
 import beans.main.PhotoDesBean;
+import utils.EnumUtil.ErrorCodeJson;
 import utils.EnumUtil.PhotoType;
-import utils.json.JSONObject;
 
 import javax.imageio.ImageIO;
 import javax.servlet.AsyncContext;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.Random;
-import java.util.concurrent.TimeUnit;
 
 /**
- * Created by leon on 2014/8/21.
+ * author: 康乐
+ * time: 2014/8/24
+ * function: 存储新上传的照片并将相关的信息存入CacheRowSet
  */
-public class SavePart implements Runnable{
+
+
+public class SavePart implements Runnable {
+
+	static long asd = 0;
+
 
 	private long ID;
 	private ArrayList<BufferedImage> newImages;
 	private ArrayList<BufferedImage> oldImages;
 	private PhotoDesBean photoDesBean;
 
-	private ArrayList<String> newImagesPaths;
-	private ArrayList<String> oldImagesPaths;
-
 	AsyncContext asyncContext;
 
-	public SavePart(ArrayList<BufferedImage> newImages, ArrayList<BufferedImage> oldImages, PhotoDesBean photoDesBean, long ID,AsyncContext asyncContext) {
+	public SavePart(ArrayList<BufferedImage> newImages, ArrayList<BufferedImage> oldImages, PhotoDesBean photoDesBean, long ID, AsyncContext asyncContext) {
 		this.newImages = newImages;
 		this.oldImages = oldImages;
 		this.photoDesBean = photoDesBean;
 		this.ID = ID;
-		oldImagesPaths = new ArrayList<String>();
-		newImagesPaths = new ArrayList<String>();
 		this.asyncContext = asyncContext;
 	}
 
 	@Override
 	public void run() {
 
-
-		try {
-			TimeUnit.SECONDS.sleep(10);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-
-
-		//  存储调整尺寸后的图片，并将位置信息存入newImagesPaths
-		for(BufferedImage bufferedImage : newImages) {
-			try {
-
-				File saveParentFile = getSaveFile(PhotoType.RESIZE_PHOTO);
-				File saveFile = new File(saveParentFile,getFileName());
-				ImageIO.write(bufferedImage, "jpg",saveFile);
-
-				// 如果是专辑，那么就存储储存专辑文件的文件夹的名字
-				if (photoDesBean.getAlbumName() != null) {
-					newImagesPaths.add(saveParentFile.getPath());
-				}else {
-					newImagesPaths.add(saveFile.getPath());
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-
-		}
-
-		//  存储调整尺寸后的图片，并将位置信息存入oldImagesPaths
-		for (BufferedImage bufferedImage : oldImages) {
-			try {
-
-				File saveParentFile = getSaveFile(PhotoType.ORIGINAL_PHOTO);
-				File saveFile = new File(saveParentFile,getFileName());
-				ImageIO.write(bufferedImage, "jpg",saveFile);
-
-				// 如果是专辑，那么就存储储存专辑文件的文件夹的名字
-				if (photoDesBean.getAlbumName() != null) {
-					oldImagesPaths.add(saveParentFile.getPath());
-				}else {
-					oldImagesPaths.add(saveFile.getPath());
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-
-		// 解析出照片位置信息，因为如果是专辑，那么存储的文件夹的名字，所以list中
-		// 所有的名字都是一样的；如果是单张照片，那么照片位置势必只有单一的一个位置
-		// 所以其大小是1，位置信息储存在0单元中，所以无论是照片还是专辑，都可以通过
-		// 0号单元中的信息确定其位置信息
 		String newPhotoPath;
-		String oldPhotoPath;
 
-		newPhotoPath = newImagesPaths.get(0);
-		oldPhotoPath = oldImagesPaths.get(0);
+		String oldPhotoPath = null;
+
+		File saveParentFile;
+
+		// 如果是专辑，那么就进行存储并将文件夹的名字赋值给相应的位置
+		if (photoDesBean.getAlbumName() != null ) {
+
+			// 获得压缩后专辑照片存储的位置
+			saveParentFile = getSaveFile(PhotoType.RESIZE_PHOTO);
+			if (saveParentFile != null) {
+
+				for (BufferedImage bufferedImage : newImages) {
+					File saveFile = new File(saveParentFile, getFileName());
+					try {
+						ImageIO.write(bufferedImage, "jpg", saveFile);
+					} catch (IOException e) {
+						sendIoError(asyncContext);
+						File[] files = saveParentFile.listFiles();
+						if (files != null) {
+							for (File file : files) {
+								file.delete();
+							}
+						}
+						e.printStackTrace();
+					}
+				}
+
+				// 将压缩后专辑的文件夹的位置存储
+				newPhotoPath = saveParentFile.getPath();
+			}else {
+				sendMkDirErrorCode(asyncContext);
+				return ;
+			}
+
+
+			// 存储原图
+			saveParentFile = getSaveFile(PhotoType.ORIGINAL_PHOTO);
+			if (saveParentFile != null) {
+
+				for (BufferedImage bufferedImage : oldImages) {
+					File saveFile = new File(saveParentFile, getFileName());
+					try {
+						ImageIO.write(bufferedImage, "jpg", saveFile);
+					} catch (IOException e) {
+						sendIoError(asyncContext);
+						//删除开始存储的新图
+						File f = new File(newPhotoPath);
+						File[] oldFiles = f.listFiles();
+						if (oldFiles != null) {
+							for (File oldPhoto : oldFiles) {
+								oldPhoto.delete();
+							}
+						}
+
+						// 删除原图
+						File[] originalFiles = saveParentFile.listFiles();
+						if (originalFiles != null) {
+							for (File file : originalFiles) {
+								file.delete();
+							}
+						}
+						e.printStackTrace();
+					}
+				}
+				// 将压缩后专辑的文件夹的位置存储
+				oldPhotoPath = saveParentFile.getPath();
+			}else {
+				// 向客户端发送错误信息
+				sendMkDirErrorCode(asyncContext);
+
+				// 原图存储出现问题，删除原来存储的新图并向客户端发送错误代码信息
+				File file = new File(newPhotoPath);
+				File[] files = file.listFiles();
+				if (files != null) {
+					for (File f : files) {
+						f.delete();
+					}
+				}
+
+			}
+		}else {
+			// 如果不是专辑，那么就是单张照片的存储
+
+			// 获得压缩后照片存储的位置
+			saveParentFile = getSaveFile(PhotoType.RESIZE_PHOTO);
+			if (saveParentFile != null) {
+				File saveFile = new File(saveParentFile, getFileName());
+				// 照片的位置信息
+				newPhotoPath = saveFile.getPath();
+				try {
+					ImageIO.write(newImages.get(0), "jpg", saveFile);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}else {
+				// 向客户端发送错误信息
+				sendMkDirErrorCode(asyncContext);
+				return ;
+			}
+
+			// 存储单张原图
+			saveParentFile = getSaveFile(PhotoType.ORIGINAL_PHOTO);
+			if (saveParentFile != null) {
+				File saveFile = new File(saveParentFile, getFileName());
+				// 照片的位置信息
+				oldPhotoPath = saveFile.getPath();
+				try {
+					ImageIO.write(oldImages.get(0), "jpg", saveFile);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}else {
+				sendMkDirErrorCode(asyncContext);
+				File file = new File(newPhotoPath);
+				file.delete();
+			}
+		}
 
 		photoDesBean.setViewPhotoPath(newPhotoPath);
 		photoDesBean.setDetailPhotoPath(oldPhotoPath);
@@ -111,45 +172,107 @@ public class SavePart implements Runnable{
 	}
 
 
+	/**
+	 * 创建保存位置的文件夹，如果是多个文件夹创建失败，将会依次删除创建好后的文件夹
+	 * 如果是单个文件夹创建失败，则返回null
+	 *
+	 * @param photoType 照片的类型
+	 * @return 如果成功创建，那么返回创建好的File对象；如果创建失败，返回null
+	 * @see utils.EnumUtil.PhotoType
+	 */
 	private File getSaveFile(PhotoType photoType) {
 
 		File savePlace = null;
 
-		int saveDirectory = (int)(ID / 1000) + 1;
+		int saveDirectory = (int) (ID / 1000) + 1;
 
-		String idFileParentName = "E:/shunjian_source/photo/" + saveDirectory + "000/" + ID;
+		String idFileParentName = "E:/shunjian_source/photo/";
 
-		File idFileParent = new File(idFileParentName);
+		File idFileParent = new File(idFileParentName, saveDirectory + "000/" + ID);
 
 		if (!idFileParent.exists()) {
 			boolean mkdirs = idFileParent.mkdirs();
+			if (!mkdirs) {
+				// 循环删除已经创建的文件夹
+				// 但是这里是不用删除，因为每一个ID都有自己的专属文件夹，就算创建了也没有什么
+				return null;
+			}
 		}
 
 		if (photoType == PhotoType.ORIGINAL_PHOTO) {
 
-			savePlace = new File(idFileParent,"originalPhoto");
+			savePlace = new File(idFileParent, "originalPhoto");
 			if (!savePlace.exists()) {
-				savePlace.mkdir();
+				boolean mkdir = savePlace.mkdir();
+				if (!mkdir) {
+					return null;
+				}
 			}
-		}else if (photoType == PhotoType.RESIZE_PHOTO) {
-			savePlace = new File(idFileParent,"resizePhoto");
+		} else if (photoType == PhotoType.RESIZE_PHOTO) {
+			savePlace = new File(idFileParent, "resizePhoto");
 			if (!savePlace.exists()) {
-				savePlace.mkdir();
+				boolean mkdir = savePlace.mkdir();
+				if (!mkdir) {
+					return null;
+				}
 			}
 		}
 
-		if (photoDesBean.getAlbumName() != null){
+		if (photoDesBean.getAlbumName() != null) {
 			String albumName = photoDesBean.getAlbumName();
-			File albumDirectory = new File(savePlace,albumName);
+			File albumDirectory = new File(savePlace, albumName);
 			if (!albumDirectory.exists()) {
-				albumDirectory.mkdir();
+				boolean mkdir = albumDirectory.mkdir();
+				if (!mkdir) {
+					return null;
+				}
 			}
 			savePlace = albumDirectory;
 		}
 		return savePlace;
 	}
 
+
+	/**
+	 * 创建名字为当前毫秒数的图片jpg文件
+	 *
+	 * @return 格式为jpg的文件名字
+	 */
 	private String getFileName() {
 		return System.currentTimeMillis() + ".jpg";
 	}
+
+	/**
+	 * 向客户端发送文件夹创建错误消息并关闭AsyncContext资源
+	 *
+	 * @param asyncContext servlet异步对象
+	 */
+	private void sendMkDirErrorCode(AsyncContext asyncContext) {
+		try {
+			asyncContext.getResponse().getWriter().write(ErrorCodeJson.MKDIRERROR.toString());
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			// 关闭AsyncContext资源
+			asyncContext.complete();
+		}
+	}
+
+
+	/**
+	 * 向客户端发送IO错误的消息并关闭AsyncContext资源
+	 * @param asyncContext servlet异步对象
+	 */
+	private void sendIoError(AsyncContext asyncContext) {
+		try {
+			asyncContext.getResponse().getWriter().write(ErrorCodeJson.IOERROR.toString());
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			// 关闭AsyncContext资源
+			asyncContext.complete();
+		}
+	}
+
+
 }
