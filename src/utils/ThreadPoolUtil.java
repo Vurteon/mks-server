f1f1343f34fe2086a6520cffe45bedaf94cf35ed
leon <1196139850@qq.com>
2014-08-24 11:33:47 +0800

    线程池工具类

--- src/utils/ThreadPoolUtil.java
+++ src/utils/ThreadPoolUtil.java
@@ -9,42 +9,56 @@ import java.util.concurrent.TimeUnit;
 
 
 /**
- *
- *
+ * author: 康乐
+ * time: 2014/8/24
+ * function: 提供创建线程池所需要的参数和线程池
  */
 
 public class ThreadPoolUtil {
 
+	// IO密集型线程池基础线程数量
 	public static final int IOCORETHREADSNUMBER = 36;
 
+	// IO密集型线程池最大线程数量
 	public static final int IOMAXTHREADSNUMBER = 56;
 
+	// IO线程密集型线程线程保持存活时间 10s
 	public static final int IOTHREADSKEEPALIVETIME = 10;
 
+	// IO线程密集型保持存活时间的单位
 	public static final TimeUnit IOTHREADSTIMEUNIT = TimeUnit.SECONDS;
 
-	public static final int IOTHREADSQUEUELENGTH = 5;
+	// IO密集型线程池的队列长度
+	public static final int IOTHREADSQUEUELENGTH = 50;
 
+	// IO密集型线程池队列
 	public static final LinkedBlockingQueue<Runnable> IOTHREADSQUEUE = new LinkedBlockingQueue<Runnable>(IOTHREADSQUEUELENGTH);
 
+	// CPU密集型线程池基础线程数量
+	public static final int CPUCORETHREADSNUMBER = 6;
 
-	public static final int CPUCORETHREADSNUMBER = 5;
+	// CPU密集型线程池最大线程数量
+	public static final int CPUMAXTHREADSNUMBER = 11;
 
-	public static final int CPUMAXTHREADSNUMBER = 9;
-
+	// CPU线程密集型线程线程保持存活时间 10s
 	public static final int CPUTHREADSKEEPALIVETIME = 60;
 
+	// CPU程密集型保持存活时间的单位
 	public static final TimeUnit CPUTHREADSTIMEUNIT = TimeUnit.MINUTES;
 
+	// CPU集型线程池的队列长度
 	public static final int CPUTHREADSQUEUELENGTH = 80;
 
+	// CPU集型线程池队列
 	public static final LinkedBlockingQueue<Runnable> CPUTHREADSQUEUE = new LinkedBlockingQueue<Runnable>(CPUTHREADSQUEUELENGTH);
 
+	// 当线程池和队列都满的时候所采取的的对于策略
 	public static final RejectedExecutionHandler HANDLER = new RejectedHandler();
 
-
+	// cpu线程池对象
 	public static ThreadPoolExecutor cpuThreadPoolExecutor;
 
+	// io线程池对象
 	public static ThreadPoolExecutor ioThreadPoolExecutor;
 
 	public static ThreadPoolExecutor getCpuThreadPoolExecutor() {
