756e6952186383b4449d702f49d94189fa80bf50
leon <1196139850@qq.com>
2014-08-29 09:42:32 +0800

    线程池管理对象

--- src/utils/ThreadPoolUtils.java
+++ src/utils/ThreadPoolUtils.java
@@ -11,10 +11,15 @@ import java.util.concurrent.TimeUnit;
 /**
  * author: 康乐
  * time: 2014/8/24
- * function: 提供创建线程池所需要的参数和线程池
+ * function: 提供创建线程池所需要的参数和线程池，需要比较注意的是：当前
+ * 的cpu线程池线程的数量最佳为cpu的数量加1，而IO线程池的线程数量最好是
+ * 当前cpu数量的2倍，至于存活时间，那么就需要根据任务的多少等因素来决定
+ *
+ * 目前io线程池的线程数量是cpu数量的8倍，存活时间是10s，这个值十分需要以
+ * 后的进一步测量.
  */
 
-public class ThreadPoolUtil {
+public class ThreadPoolUtils {
 
 	// IO密集型线程池基础线程数量
 	public static final int IOCORETHREADSNUMBER = 36;
@@ -61,19 +66,39 @@ public class ThreadPoolUtil {
 	// io线程池对象
 	public static ThreadPoolExecutor ioThreadPoolExecutor;
 
+	/**
+	 * cpu线程池getter
+	 * @return 当前cpu线程池对象
+	 */
 	public static ThreadPoolExecutor getCpuThreadPoolExecutor() {
+
 		return cpuThreadPoolExecutor;
 	}
 
+	/**
+	 * io线程池getter
+	 * @return 当前cpu线程池对象
+	 */
 	public static ThreadPoolExecutor getIoThreadPoolExecutor() {
+
 		return ioThreadPoolExecutor;
 	}
 
+	/**
+	 * cpu线程池setter
+	 * @param cpuThreadPoolExecutor 需要设置的cpu线程池对象
+	 */
 	public static void setCpuThreadPoolExecutor(ThreadPoolExecutor cpuThreadPoolExecutor) {
-		ThreadPoolUtil.cpuThreadPoolExecutor = cpuThreadPoolExecutor;
+
+		ThreadPoolUtils.cpuThreadPoolExecutor = cpuThreadPoolExecutor;
 	}
 
+	/**
+	 * io线程池setter
+	 * @param ioThreadPoolExecutor 需要设置的io线程池对象
+	 */
 	public static void setIoThreadPoolExecutor(ThreadPoolExecutor ioThreadPoolExecutor) {
-		ThreadPoolUtil.ioThreadPoolExecutor = ioThreadPoolExecutor;
+
+		ThreadPoolUtils.ioThreadPoolExecutor = ioThreadPoolExecutor;
 	}
 }
