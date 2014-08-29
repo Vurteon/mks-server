99eaf5f35f7929665ff9e7617857e57991474250
leon <1196139850@qq.com>
2014-08-29 09:45:28 +0800

    Error Code

--- src/utils/EnumUtil/ErrorCode.java
+++ src/utils/EnumUtil/ErrorCode.java
@@ -1,15 +1,22 @@
 package utils.EnumUtil;
 
 /**
- * Created by leon on 2014/8/22.
+ * author: 康乐
+ * time: 2014/8/28
+ * function: 提供对客户端错误信息的返回码
  */
+
 public class ErrorCode {
 
 	// 客户端构造的json格式错误
 	public static final int JSONFORMATERROR = 0;
 
+	// 服务器IO错误
 	public static final int IOERROR = 1;
 
+	// 服务器文件系统构建文件出错
 	public static final int MKDIRERROR = 2;
+
+
 
 }
