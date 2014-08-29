9f0ed404aae08764a3873a7fc679800dda8e893c
leon <1196139850@qq.com>
2014-08-29 09:46:13 +0800

    Time Builder

--- src/utils/db/TimeBuilder.java
+++ src/utils/db/TimeBuilder.java
@@ -4,13 +4,21 @@ import java.sql.Timestamp;
 import java.util.Date;
 
 /**
- * Created by leon on 2014/8/27.
+ * author: 康乐
+ * time: 2014/8/28
+ * function: 更加Date类所提供的long值来得到sql中的Timestamp时间对象
  */
 public class TimeBuilder {
 
+	// 需要的时间的对象
 	public static final Date time = new Date();
 
+	/**
+	 * 返回当前时间的Timestamp对象
+	 * @return 单签时间的Timestamp对象
+	 */
 	public static Timestamp getLocalTime() {
+		//
 		return new Timestamp(time.getTime());
 	}
 
