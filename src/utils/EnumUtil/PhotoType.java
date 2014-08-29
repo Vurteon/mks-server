c4fd7a3438faff134c4de169e256f5f39f9e3dd4
leon <1196139850@qq.com>
2014-08-29 09:44:36 +0800

    Photo Type Enum

--- src/utils/EnumUtil/PhotoType.java
+++ src/utils/EnumUtil/PhotoType.java
@@ -1,11 +1,16 @@
 package utils.EnumUtil;
 
 /**
- * Created by leon on 2014/8/22.
+ * author: 康乐
+ * change-time :2014/8/28
+ * function: 图片本身所属的带下类型，目前有上传的原图和用户在个人中心浏览时的压缩小图
  */
+
 public enum PhotoType {
 
+	// 上传图片的原图
 	ORIGINAL_PHOTO,
 
+	// 个人中心浏览时被压缩后的小图
 	RESIZE_PHOTO
 }
