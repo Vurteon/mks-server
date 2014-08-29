ace8dc43de009eb575292a86f9743373a08c01aa
leon <1196139850@qq.com>
2014-08-29 09:45:00 +0800

    Error Code Json

--- src/utils/EnumUtil/ErrorCodeJson.java
+++ src/utils/EnumUtil/ErrorCodeJson.java
@@ -4,12 +4,17 @@ import utils.CreateJson;
 import utils.json.JSONObject;
 
 /**
- * Created by leon on 2014/8/22.
+ * author: 康乐
+ * time: 2014/8/28
+ * function: 提供ErrorJson对象
  */
+
 public class ErrorCodeJson {
 
+	// IO错误代码的json对象
 	public static final JSONObject IOERROR = CreateJson.getJsonObject("{'errorCode':" + ErrorCode.IOERROR + "}");
 
+	// 服务器文件系统错误代码的json对象
 	public static final JSONObject MKDIRERROR = CreateJson.getJsonObject("{'errorCode':" + ErrorCode.MKDIRERROR + "}");
 
 
