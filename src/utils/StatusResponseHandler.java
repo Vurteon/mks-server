59f26a4d4953daf2733b642567541eb0a17120e6
leon <1196139850@qq.com>
2014-11-02 11:36:02 +0800

    添加了注释，设置了返回字符集为UTF-8

--- src/utils/StatusResponseHandler.java
+++ src/utils/StatusResponseHandler.java
@@ -1,5 +1,6 @@
 package utils;
 
+import utils.json.JSONArray;
 import utils.json.JSONObject;
 
 import javax.servlet.http.HttpServletResponse;
@@ -7,14 +8,24 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 /**
- * Created by leon on 14-10-14.
+ * author:康乐
+ * time:2014/11/2
+ * function:向客户端发送服务器所提供的返回数据
+ *
  */
+
 public class StatusResponseHandler {
 
-
-	public static void sendStatus(String key, String value,HttpServletResponse httpServletResponse) {
+	/**
+	 * 向客户端发送单个键值对，一般用于表示状态信息。发送完成，释放输出资源
+	 * @param key key
+	 * @param value value
+	 * @param httpServletResponse 客户端所对应的httpServletResponse对象
+	 */
+	public static void sendStatus(String key, String value, HttpServletResponse httpServletResponse) {
 
 		httpServletResponse.setHeader("Content-Type", "application/json");
+		httpServletResponse.setCharacterEncoding("UTF-8");
 		StringBuilder sb = new StringBuilder();
 		sb.append("{");
 		sb.append("'");
@@ -33,31 +44,66 @@ public class StatusResponseHandler {
 			pw.write(sb.toString());
 		} catch (IOException e) {
 			e.printStackTrace();
-		}finally {
+		} finally {
 			releaseIO(pw);
 		}
 
 	}
 
 
+	/**
+	 * 向客户端发送一个json对象，用户可选发送完成后是否释放输出资源
+	 * @param jsonObject    需要发送的json对象
+	 * @param httpServletResponse   客户端所对应的httpServletResponse对象
+	 * @param isRelease     是否需要释放输出资源。如果为true，释放资源；否则，不释放资源，以便继续发送其他返回数据
+	 */
+	public static void sendStatus(JSONObject jsonObject, HttpServletResponse httpServletResponse, boolean isRelease) {
 
-	public static void sendStatus(JSONObject jsonObject,HttpServletResponse httpServletResponse) {
-
+		httpServletResponse.setHeader("Content-Type", "application/json");
+		httpServletResponse.setCharacterEncoding("UTF-8");
 		PrintWriter pw = null;
 		try {
 			pw = httpServletResponse.getWriter();
 			pw.write(jsonObject.toString());
-			releaseIO(pw);
+			if (isRelease)
+				releaseIO(pw);
 		} catch (IOException e) {
 			e.printStackTrace();
-		}finally {
-			releaseIO(pw);
+		} finally {
+			if (isRelease)
+				releaseIO(pw);
 		}
-
-
 	}
 
-	private static void releaseIO (PrintWriter printWriter) {
+
+	/**
+	 * 向客户端发送一个json对象，用户可选发送完成后是否释放输出资源
+	 * @param jsonArray    需要发送的json对象数组
+	 * @param httpServletResponse   客户端所对应的httpServletResponse对象
+	 * @param isRelease     是否需要释放输出资源。如果为true，释放资源；否则，不释放资源，以便继续发送其他返回数据
+	 */
+	public static void sendStatus(JSONArray jsonArray, HttpServletResponse httpServletResponse, boolean isRelease) {
+		httpServletResponse.setHeader("Content-Type", "application/json");
+		httpServletResponse.setCharacterEncoding("UTF-8");
+		PrintWriter pw = null;
+		try {
+			pw = httpServletResponse.getWriter();
+			pw.write(jsonArray.toString());
+			if (isRelease)
+				releaseIO(pw);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (isRelease)
+				releaseIO(pw);
+		}
+	}
+
+	/**
+	 * 释放参数所指向的输出资源
+	 * @param printWriter   输出资源
+	 */
+	private static void releaseIO(PrintWriter printWriter) {
 		if (printWriter != null) {
 			printWriter.close();
 		}
