4fe71bde6a86abd5476f19a0bf230d1b4c9707e2
leon <1196139850@qq.com>
2014-11-03 17:31:12 +0800

    增加了也可以请求doGet来获得信息

--- src/servlet/user/GetUserInfoServlet.java
+++ src/servlet/user/GetUserInfoServlet.java
@@ -51,6 +51,6 @@ public class GetUserInfoServlet extends HttpServlet {
 	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+		doPost(request,response);
 	}
 }
