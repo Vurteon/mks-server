ee8aa4aff407a915ba6ced78d39575df713cbd56
leon <1196139850@qq.com>
2014-11-14 15:51:00 +0800

    添加了三个数量指标的获取 和 获得指定的ID的信息的model方法

--- src/model/user/UserInfoManger.java
+++ src/model/user/UserInfoManger.java
@@ -1,6 +1,7 @@
 package model.user;
 
 import dao.user.UserInfoDao;
+import utils.db.ReleaseSource;
 import utils.json.JSONArray;
 import utils.json.JSONObject;
 
@@ -33,8 +34,31 @@ public class UserInfoManger {
 				buildUserInfo(cachedRowSet,jsonArray);
 			}while (cachedRowSet.next());
 		}
+		ReleaseSource.releaseSource(cachedRowSet);
 		return jsonArray;
 	}
+
+
+	/**
+	 * 从DAO接口获取CachedRowSet对象，转化为json数组并返回给上一层
+	 * @param ID 用户所关注的人的ID号
+	 * @return 带有用户个人信息的json数组
+	 * @throws SQLException
+	 */
+	public static JSONObject getUsersInfo (int ID) throws SQLException {
+
+		CachedRowSet cachedRowSet = UserInfoDao.getUserInfo(ID);
+
+		JSONArray jsonArray = new JSONArray();
+		if (cachedRowSet.first()) {
+			buildUserInfo(cachedRowSet,jsonArray);
+		}
+		ReleaseSource.releaseSource(cachedRowSet);
+		return jsonArray.getJSONObject(0);
+	}
+
+
+
 
 	/**
 	 * 依据CachedRowSet构建json对象，并将其放入json数组
@@ -44,19 +68,41 @@ public class UserInfoManger {
 	 */
 	private static void buildUserInfo (CachedRowSet cachedRowSet, JSONArray jsonArray) throws SQLException {
 
+		int ID = cachedRowSet.getInt("ID");
 		String name = cachedRowSet.getString("name");
 		String brief_intro = cachedRowSet.getString("brief_intro");
+		String bg_photo = cachedRowSet.getString("bg_photo");
 		String main_head_photo = cachedRowSet.getString("main_head_photo");
 		String home_head_photo = cachedRowSet.getString("home_head_photo");
 
 		JSONObject jsonObject = new JSONObject();
+		jsonObject.put("ID", ID);
 		jsonObject.put("name",name);
 		jsonObject.put("brief_intro",brief_intro);
+		jsonObject.put("bg_photo",bg_photo);
 		jsonObject.put("main_head_photo",main_head_photo);
 		jsonObject.put("home_head_photo",home_head_photo);
 
 		jsonArray.put(jsonObject);
 	}
 
+	/**
+	 * 获取指定的ID的三个数量指标
+	 * @param ID ID
+	 * @return 返回指定的ID的三个数量指标的json对象
+	 */
+	public static JSONObject getThreeNum (int ID) throws SQLException {
 
+		int photosNumber = UserInfoDao.getUserPhotoNum(ID);
+		int fansNum = UserInfoDao.getUserFansNum(ID);
+		int followingsNum = UserInfoDao.getUserFollowingNum(ID);
+
+		JSONObject jsonObject = new JSONObject();
+
+		jsonObject.put("photosNumber", photosNumber);
+		jsonObject.put("fansNum", fansNum);
+		jsonObject.put("followingsNum", followingsNum);
+
+		return jsonObject;
+	}
 }
