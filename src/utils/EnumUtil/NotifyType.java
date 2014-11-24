010a650d85f6a36fcf0f46cdfebc1fbf51652bff
leon <1196139850@qq.com>
2014-11-24 23:00:46 +0800

    三种推送消息类型

---
+++ src/utils/EnumUtil/NotifyType.java
@@ -0,0 +1,14 @@
+package utils.EnumUtil;
+
+/**
+ * Created by leon on 2014/11/24.
+ */
+public class NotifyType {
+
+	public static final String COMMENT = "COMMENT";
+
+	public static final String LIKE = "LIKE";
+
+	public static final String FOLLOW = "FOLLOW";
+
+}
